3b442c8fd463f96cee8adc26bc83de38cc5f820b
Thomas JARRAND <thomas.jarrand@elao.com>
2014-06-06 13:01:31 +0200

    Base structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,12 @@ var gulp   = require('gulp'),
     meta   = require('./package.json');
 
 var srcDir = './src/',
-    distDir = './dist/',
+    jsDir = './web/js/',
+    cssDir = './web/css/',
+    expose = [
+        './bower_components/almond/almond.js',
+        './bower_components/paper/dist/paper-full.min.js',
+    ],
     vendors = [
         './bower_components/tom32i-event-emitter.js/dist/event-emitter.min.js',
         './bower_components/tom32i-option-resolver.js/dist/option-resolver.min.js'
@@ -25,12 +30,18 @@ gulp.task('jshint', function() {
         .pipe(jshint.reporter());
 });
 
+gulp.task('expose', function() {
+    gulp.src(expose)
+        .pipe(concat('dependencies.js'))
+        .pipe(gulp.dest(jsDir));
+});
+
 gulp.task('full', function() {
     gulp.src(vendors.concat([srcDir + '**/*.js']))
         .pipe(concat(meta.name + '.js'))
         .pipe(uglify())
         .pipe(header(banner, meta))
-        .pipe(gulp.dest(distDir));
+        .pipe(gulp.dest(jsDir));
 });
 
 gulp.task('min', function(){
@@ -38,11 +49,12 @@ gulp.task('min', function(){
         .pipe(concat(meta.name + '.min.js'))
         .pipe(uglify())
         .pipe(header(banner, meta))
-        .pipe(gulp.dest(distDir));
+        .pipe(gulp.dest(jsDir));
 });
 
-gulp.task('watch', ['default'], function () {
-    gulp.watch(srcDir + '**/*.js', ['default']);
+gulp.task('watch', ['dev'], function () {
+    gulp.watch(srcDir + '**/*.js', ['dev']);
 });
 
-gulp.task('default', ['jshint', 'full', 'min']);
+gulp.task('default', ['jshint', 'expose', 'min']);
+gulp.task('dev', ['jshint', 'full']);
