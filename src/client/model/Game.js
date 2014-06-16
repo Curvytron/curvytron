866a610d43070d65ccd3b906329ffc7f064332d2
Jeremy Fagis <jeremy@fagis.fr>
2014-06-16 11:39:04 +0200

    Improved UI.

--- src/client/model/Game.js
+++ src/client/model/Game.js
@@ -57,7 +57,7 @@ Game.prototype.onResize = function()
 {
     var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
-    var width = Math.min(x - 300 - 12, y);
+    var width = Math.min(x - 300 - 8, y);
 
     paper.view.viewSize.width = width;
     paper.view.viewSize.height = width;
