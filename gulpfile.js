6322fcaf114bd2efe82279b3cb4dc16b30aeb133
lutangar <johan.dufour@gmail.com>
2014-06-19 16:57:15 +0200

    add lodash as a depdency in bower

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ var gulp      = require('gulp'),
             './bower_components/tom32i-event-emitter.js/dist/event-emitter.min.js',
             './bower_components/tom32i-option-resolver.js/dist/option-resolver.min.js',
             './bower_components/tom32i-gamepad.js/dist/gamepad.min.js',
-            './bower_components/tom32i-key-mapper.js/dist/key-mapper.min.js'
+            './bower_components/tom32i-key-mapper.js/dist/key-mapper.min.js',
+            './bower_components/lodash/dist/lodash.min.js'
         ],
         recipes = {
             server: require('./recipes/server.json'),
@@ -96,10 +97,8 @@ gulp.task('server', function() {
 
 gulp.task('nodemon', function () {
     nodemon({ watch: recipes.server.files, ext: 'js', script: 'bin/curvytron.js', restartable: "rs"})
-        .on('change', ['server'])
-        .on('restart', function () {
-            console.log('restarted!')
-        })
+        .on('change', ['server', 'front-expose', 'front-full'])
+        .on('restart', ['default'])
 })
 
 gulp.task('sass-full', function() {
@@ -126,4 +125,4 @@ gulp.task('watch', ['dev', 'views', 'sass-full'], function () {
 });
 
 gulp.task('default', ['jshint', 'server', 'front-expose', 'views', 'front-min', 'sass-min']);
-gulp.task('dev', ['jshint', 'server', 'views', 'front-full', 'sass-full']);
+gulp.task('dev', ['jshint', 'server', 'front-expose', 'views', 'front-full', 'sass-full']);
