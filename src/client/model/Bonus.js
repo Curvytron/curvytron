33ea6402c81fd713b300b9092c6ac9aeeea19e2c
lutangar <johan.dufour@gmail.com>
2014-06-19 16:59:36 +0200

    add bonus client model

---
+++ src/client/model/Bonus.js
@@ -0,0 +1,46 @@
+/**
+ * Bonus
+ *
+ * @param color
+ * @param radius
+ */
+function Bonus(name, color, radius)
+{
+    BaseBonus.call(this, name, color, radius);
+
+    this.path = null;
+
+}
+
+Bonus.prototype = Object.create(BaseBonus.prototype);
+
+/**
+ * Create path
+ */
+Bonus.prototype.createPath = function()
+{
+    this.path = new paper.Shape.Circle({
+        center: new paper.Point(this.position[0] * paper.sceneScale, this.position[1] * paper.sceneScale),
+        radius: this.radius * paper.sceneScale,
+        fillColor: this.color,
+        fullySelected: false
+    });
+};
+
+/**
+ * Pop
+ *
+ * @param {Array} point
+ */
+Bonus.prototype.pop = function()
+{
+    this.createPath();
+};
+
+/**
+ * Clear
+ */
+Bonus.prototype.clear = function()
+{
+    this.path.remove();
+};
