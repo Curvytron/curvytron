c7622a95acde9bda4f96c48afb0033e238cd46e3
tom32i <thomas.jarrand@gmail.com>
2014-06-19 21:48:17 +0200

    Fixed game border

--- src/client/model/Game.js
+++ src/client/model/Game.js
@@ -105,9 +105,10 @@ Game.prototype.onResize = function()
 {
     var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
-    var width = Math.min(x - 300 - 8, y);
+    var width = Math.min(x - 300 - 8, y - 8);
 
-    paper.view.viewSize.width = width;
+    paper.view.viewSize.width  = width;
     paper.view.viewSize.height = width;
+
     paper.sceneScale = width / this.size;
 };
