9658cd5efdda151c134585993366684266de9732
lutangar <johan.dufour@gmail.com>
2014-06-21 17:12:42 +0200

    add a method to check if a bonus is taken by given avatar

--- src/server/model/Bonus.js
+++ src/server/model/Bonus.js
@@ -40,3 +40,17 @@ Bonus.prototype.clear = function()
     BaseBonus.prototype.clear.call(this);
     this.emit('clear');
 };
+
+Bonus.prototype.isTakenBy = function (avatar) {
+    if (
+        this.active &&
+        Island.circlesTouch(
+            [avatar.head[0], avatar.head[1], avatar.radius, avatar.mask],
+            [this.position[0], this.position[1], this.radius, 0]
+        )
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+};
