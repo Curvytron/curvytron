08a8b711c19529cebbe9bb9f0c43b924386e1ede
Jonathan Jalouzot <jjalouzot@groupe-exp.com>
2014-07-18 14:35:14 +0200

    Add many bonus

---
+++ src/server/model/Bonus/SmallerEnemyBonus.js
@@ -0,0 +1,35 @@
+/**
+ * Godzilla Enemy Bonus
+ *
+ * @param {Array} position
+ */
+function SmallerEnemyBonus(position)
+{
+    Bonus.call(this, position);
+}
+
+SmallerEnemyBonus.prototype = Object.create(Bonus.prototype);
+
+SmallerEnemyBonus.prototype.type   = 'smaller';
+SmallerEnemyBonus.prototype.affect = 'enemy';
+SmallerEnemyBonus.prototype.step   = BaseAvatar.prototype.defaultRadius;
+
+/**
+ * On
+ */
+SmallerEnemyBonus.prototype.on = function()
+{
+    for (var i = this.target.length - 1; i >= 0; i--) {
+        this.target[i].setRadius(this.target[i].radius - this.step);
+    }
+};
+
+/**
+ * Off
+ */
+SmallerEnemyBonus.prototype.off = function()
+{
+    for (var i = this.target.length - 1; i >= 0; i--) {
+        this.target[i].setRadius(this.target[i].radius + this.step);
+    }
+};
