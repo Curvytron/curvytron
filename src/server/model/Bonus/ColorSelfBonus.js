dcf78258be5d055755a9d785acbc1f50dca0668c
Jonathan Jalouzot <jjalouzot@groupe-exp.com>
2014-07-18 14:35:14 +0200

    Add many bonus

---
+++ src/server/model/Bonus/ColorSelfBonus.js
@@ -0,0 +1,43 @@
+/**
+ * Godzilla Enemy Bonus
+ *
+ * @param {Array} position
+ */
+function ColorSelfBonus(position)
+{
+    Bonus.call(this, position);
+}
+
+ColorSelfBonus.prototype = Object.create(Bonus.prototype);
+
+ColorSelfBonus.prototype.type   = 'color';
+ColorSelfBonus.prototype.affect = 'self';
+ColorSelfBonus.prototype.step   = BaseAvatar.prototype.defaultRadius;
+
+/**
+ * On
+ */
+ColorSelfBonus.prototype.on = function()
+{
+    this.target.player.setColor(this.getRandomColor());
+};
+
+/**
+ * Off
+ */
+ColorSelfBonus.prototype.off = function()
+{
+};
+
+/**
+ * Get random Color
+ *
+ * @return {String}
+ */
+ColorSelfBonus.prototype.getRandomColor = function()
+{
+    var code = Math.floor(Math.random()*16777215).toString(16),
+        miss = 6 - code.length;
+
+    return '#' + code + (miss ? new Array(miss +1).join('0') : '');
+};
