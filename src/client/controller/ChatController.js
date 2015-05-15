0569b22feb04306e71fdfa0c2bd29a986b9a34ba
tom32i <thomas.jarrand@gmail.com>
2015-05-15 18:51:33 +0200

    Added mute feature to the chat

--- src/client/controller/ChatController.js
+++ src/client/controller/ChatController.js
@@ -10,15 +10,18 @@ function ChatController($scope, chat)
     this.chat   = chat;
 
     this.onLoaded    = this.onLoaded.bind(this);
+    this.mute        = this.mute.bind(this);
     this.applyScope  = this.applyScope.bind(this);
     this.digestScope = this.digestScope.bind(this);
 
     this.$scope.onLoaded       = this.onLoaded;
+    this.$scope.mute           = this.mute;
     this.$scope.messages       = this.chat.messages;
     this.$scope.currentMessage = this.chat.message;
     this.$scope.submitTalk     = this.chat.talk;
 
     this.chat.on('message', this.digestScope);
+    this.chat.on('filtered', this.digestScope);
 }
 
 /**
@@ -27,6 +30,22 @@ function ChatController($scope, chat)
 ChatController.prototype.onLoaded = function ()
 {
     this.chat.setElement(document.getElementById('chat-feed'));
+};
+
+/**
+ * Mute client from the given message
+ *
+ * @param {MessagePlayer} message
+ */
+ChatController.prototype.mute = function (message)
+{
+    if (this.chat.toggleMute(message.client)) {
+        this.chat.addMessage(new MessageMute(message.client, message.player));
+    } else {
+        this.chat.removeMessage(message);
+    }
+
+    this.digestScope();
 };
 
 /**
